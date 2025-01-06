0b60e5ca9efcc7f49cffa68912b7c35e7c0b1bfc
Jatin Kumar <110776364+Jatin7k@users.noreply.github.com>
2025-01-06 21:36:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,16 +12,12 @@ const PORT = process.env.PORT;
 //middlewares
 app.use(express.json());
 app.use(
-  cors({
-    origin: ["http://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
+  cors()
 );
 
 //routes
 readdirSync("./routes").map((route) =>
-  app.use("/", require("./routes/" + route))
+  app.use("/api/v1/", require("./routes/" + route))
 );
 
 const server = () => {

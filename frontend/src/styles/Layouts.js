2ddb34f9d368ff7f7936818361b68083ba79097e
Jatin Kumar <110776364+Jatin7k@users.noreply.github.com>
2025-01-06 22:01:39 +0530

    Update Layouts.js

--- frontend/src/styles/Layouts.js
+++ frontend/src/styles/Layouts.js
@@ -5,6 +5,20 @@ export const MainLayout = styled.div`
     height: 100%;
     display: flex;
     gap: 2rem;
+     @media (max-width: 1024px) {
+    padding: 1.5rem;
+    border-radius: 24px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+    border-radius: 16px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 0.5rem;
+    border-radius: 8px;
+  }
 `;
 
 export const InnerLayout = styled.div`
